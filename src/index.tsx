a87c39b031da73784078555e299093b92e40ea6f
Tomas Nilsson <none@none>
2023-04-10 10:20:24 +0200

    Package updates (including React 18)

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,16 @@
 import "./index.css";
-
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 import { BrowserRouter } from "react-router-dom";
 
-import App from "./App";
-
-
-ReactDOM.render(
-	<React.StrictMode>
-		<BrowserRouter>
-			<App />
-		</BrowserRouter>
-	</React.StrictMode>,
-	document.getElementById('root'),
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement,
+);
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
 );
