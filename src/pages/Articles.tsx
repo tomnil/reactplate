6f470340dead48f32d5a3d523d98461df62cb049
Tomas Nilsson <none@none>
2023-04-10 10:19:34 +0200

    Add key to list

--- src/pages/Articles.tsx
+++ src/pages/Articles.tsx
@@ -1,22 +1,18 @@
-import React from "react";
 import { Link } from "react-router-dom";
-
 import styles from "./Articles.module.css";
 import { fakeDB } from "./fakedb";
-
 
 const Articles = (): JSX.Element => {
 
 	return <div className={styles.container}>
 		<h1>Articles</h1>
 		<ul>
-			{fakeDB.map(article => <>
-				<li>
-					<Link to={`/article/${article.ID}`}>
-						{article.HeadLine}
-					</Link>
-				</li>
-			</>)}
+			{fakeDB.map(article => <li key={article.ID}>
+				<Link to={`/article/${article.ID}`}>
+					{article.HeadLine}
+				</Link>
+			</li>,
+			)}
 		</ul>
 	</div >;
 
